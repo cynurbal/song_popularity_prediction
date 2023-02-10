c45fdce0b504ffd992d114dddcc2f7281a53a990
cynurbal <cbautistaa@gmail.com>
2023-02-10 22:41:15 +0100

    Actualizacion version final

--- README.md
+++ README.md
@@ -1,7 +1,55 @@
 # song_popularity_prediction
 
-Prediction of the popularity of a song using audio features from Spotify Data *(checking other sources)*.
+Predicción de la popularidad de una canción utilizando características de audio de Spotify Data.
 
-The characteristics of the songs will be extracted from the [Spotify API](https://developer.spotify.com/documentation/web-api/)
+Las características de las canciones se extraerán de la [API de Spotify](https://developer.spotify.com/documentation/web-api/)
 
-For the analysis, common features will be identified among the most popular songs that can be the input to predict if a song will successfull.
+Para el análisis, se identificarán características comunes entre las canciones más populares que pueden ser la entrada para predecir la popularidad de una canción
+
+
+```
+### Cuenta  Spotify
+
+Desde mi cuenta de Spotify he registrado una aplización para poder utilizar su API web (https://developer.spotify.com/documentation/general/guides/app-settings/):
+
+Los notebooks que se conectan a la API de Spotify necesitan leer el fichero *settings.env* que contiene en Client ID y el Cliente Secret necesarios para la autentificación
+
+El fichero *settings.env* no se ha se ha subido a github, pero lo encontrareis en la carpeta del proyecto compartida. 
+
+Este fichero debe colocarse en la ruta raíz del proyecto y en la carpeta spotify_model_predict
+
+
+# Guía de ejecución
+
+Para replicar el proyecto, ejecute los siguientes **jupyter notebooks** en el orden especificado.
+
+0. **[Requisitos de instalación](https://github.com/cynurbal/song_popularity_prediction/blob/main/0-instalar.ipynb)**
+Instalacion de librerías necesarias para el proyecto (spotipy, streamlit)
+
+1. **[Extracción de canciones top](https://github.com/cynurbal/song_popularity_prediction/blob/main/1-extract_top_song_spotify.ipynb)**
+
+Obtención de canciones dentro del Top Semanal Global (https://charts.spotify.com/charts/view/regional-global-weekly/latest)
+
+2. **[Extracción de canciones no top](https://github.com/cynurbal/song_popularity_prediction/blob/main/2-extract_no_top_song_spotify.ipynb)**
+
+Otención de canciones random dentro del mercado global
+
+3. **[Extracción de caracteristicas de canciones](https://github.com/cynurbal/song_popularity_prediction/blob/main/3-extract_features_song_spotify.ipynb)**
+
+Acceso a la API de Spotify para extraer información de la canción, características de audio, información de los artistas
+
+4. **[Preparación de los datos](https://github.com/cynurbal/song_popularity_prediction/blob/main/4-featuring_engineering.ipynb)**
+
+Análisis exploratorio de los datos y transformaciones sobre los campos
+
+5. **[Modelado](https://github.com/cynurbal/song_popularity_prediction/blob/main/5-modeling.ipynb)**
+
+Análisis de modelos de aprendizaje automatizado y evaluación de métricas. Generación de pickle del modelo
+
+6. **[Aplicación de usuario](https://github.com/cynurbal/song_popularity_prediction/blob/main/spotify_model_predict/spotify_predict.py)**
+
+Aplicación usuario desarollada en Streamlit
+
+Para ejecutar la aplicación deberá ejecutar el siguiente comando en su terminal desde la carpeta spotify_model_predict
+
+```streamlit run spotify_predict.py
--- spotify_model_predict/spotify_predict.py
+++ spotify_model_predict/spotify_predict.py
@@ -3,21 +3,22 @@ import pandas as pd
 import numpy as np
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import polarplot
 import pickle
+import json
 
-#variable entorno
-cid ="7b90adf482154140bb2ff823fddff587"
-secret = "7b03b5d482a84fd08aa92a9051c8e8e8"
-
-# extraer archivo pickle
+# lectura archivo pickle
 with open('gb_clf.pkl', 'rb') as li:
-    gb_clf = pickle.load(li)
+    model = pickle.load(li)
+
+# Leyendo credenciales de Spotify web API del fichero settings.env
+with open('settings.env') as f:
+    env_vars = json.loads(f.read())
+
 
 def main():  
 
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=cid, 
-                                                                                client_secret=secret, requests_timeout = 100))
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=env_vars['cid'], 
+                                                                                client_secret=env_vars['secret'], requests_timeout = 100))
 
     st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Spotify_logo_vertical_black.jpg/1200px-Spotify_logo_vertical_black.jpg', width=60)
     st.header('Prediciendo Popularidad de una Canción - Spotify')
@@ -30,11 +31,11 @@ def main():
 
 
     search_results = []
+    search_tracks = []
     artists = []
     
     if search_keyword is not None and len(str(search_keyword)) > 0:
         if search_selected == 'Canción':
-            #st.write("Inicia la búsqueda de canciones")
             tracks = spotify.search(q='track:'+ search_keyword,type='track', limit=20)
             tracks_list = tracks['tracks']['items']
             if len(tracks_list) > 0:
@@ -43,23 +44,21 @@ def main():
 
        
         elif search_selected == 'Artista':
-            #st.write("Inicia la búsqueda de artistas")
             artists = spotify.search(q='artist:'+ search_keyword, type='artist', limit=20)
             artists_list = artists['artists']['items']
             if len(artists_list) > 0:
                 for artist in artists_list:
                     search_results.append(artist['name'])
             
-    selected_album = None
+    selected_track = None
     selected_artist = None
 
     if search_selected == 'Canción':
         selected_track = st.selectbox("Seleccione una canción: ", search_results)
     elif search_selected == 'Artista':
-        selected_artist = st.selectbox("Seleccione un artista: ", search_results)
-    elif search_selected == 'Album':
-        selected_album = st.selectbox("Select your album: ", search_results)        
+        selected_artist = st.selectbox("Seleccione un artista: ", search_results) 
 
+    # busquedad por cancion
     if selected_track is not None and len(tracks) > 0:
         tracks_list = tracks['tracks']['items']
         track_id = None
@@ -74,26 +73,43 @@ def main():
                     preview_url = track['preview_url']
         
         if track_id is not None:
+            # se extraen caracteristiscas de la cancion
             track_features  = spotify.audio_features(track_id) 
             if track_features[0] is not None:
                 df = pd.DataFrame(track_features, index=[0])
                 df_features = df.loc[: ,['danceability', 'energy', 'instrumentalness', 'speechiness', 'liveness', 'loudness', 'acousticness', 'valence', 
                             'duration_ms', 'key', 'mode', 'tempo', 'time_signature']]
-                df_track_info = pd.json_normalize(spotify.track(track_id))[['explicit', 'popularity']]
-                df_track_followers = pd.DataFrame(data=artist_followers, columns=['followers_artist_1'])
-                df_features_final = pd.concat([df_features, df_track_followers, df_track_info], axis=1)
+                df_track_info = pd.json_normalize(spotify.track(track_id))[['explicit', 'popularity','artists','album.release_date']]
+                df_track_info['release_year'] = df_track_info['album.release_date'].str[0:4]
+                
+                df_artists = df_track_info['artists'][0]
+                artist_id = pd.json_normalize(df_artists)['id'][0]
+                df_artist_info = pd.json_normalize(spotify.artist(artist_id))
+                df_artist_followers = df_artist_info[['followers.total']]
+                df_artist_followers.columns= ['followers_artist_1']
+
+                df_features_final = pd.concat([df_features, df_track_info[['explicit', 'popularity', 'release_year']], df_artist_followers], axis=1)
+
+                def predict():
+                    #llamada a modelo de prediccion
+                    predict_model(df_features_final)
+                
                 with st.container():
                     col1, col2 = st.columns((1,7))
                     with col1:
                         st.image(img_album, width=80)
                     with col2:
                         st.audio(preview_url, format="audio/mp3") 
-                st.dataframe(df_features)
+                       
+                st.dataframe(df_features_final)
+                st.button('predecir', key=track['id'], on_click=predict)
+                
             else:
                 st.write("La canción no tiene análisis de audio")      
         else:
             st.write("Por favor seleccione una canción de la lista")
 
+    # busqueda por artista
     elif selected_artist is not None and len(artists) > 0:
         artists_list = artists['artists']['items']
         artist_id = None
@@ -101,119 +117,125 @@ def main():
         artist_followers = []
         selected_artist_choice = None
 
+        
         if len(artists_list) > 0:
             for artist in artists_list:
                 if selected_artist == artist['name']:
                     artist_id = artist['id']
                     artist_uri = artist['uri']
-                    artist_followers.append(artist['followers']['total'])
-        
+                    artist_followers = artist['followers']['total']
+              
         if artist_id is not None:
-            artist_uri = 'spotify:artist:' + artist_id
-            top_songs_result = spotify.artist_top_tracks(artist_id)    
-            df_features_final =  None
+            # listado de canciones a seleccionar
+            top_songs_result = spotify.artist_top_tracks(artist_id)  
+            tracks_list = top_songs_result['tracks']
+            if len(top_songs_result) > 0:
+                for track in tracks_list:
+                    search_tracks.append(track['name'] + "+" + track['id'])
+            
+            selected_track = st.selectbox("Seleccione una canción: ", search_tracks) 
 
-            for track in top_songs_result['tracks']:
+            if (selected_track is not None):
+                # busqueda de caracteristicas de cancion seleccionada
+                track_id = selected_track.split("+")[1]           
+                track = spotify.track(track_id)
+
                 with st.container():
                     col1, col2, col3 = st.columns((4,4,2))
                     col11, col12 = st.columns((10,2))
                     col21, col22 = st.columns((11,1))
                     col31, col32 = st.columns((11,1))
-                    #col1.write(track['id'])
+                        
                     col1.image(track['album']['images'][0]['url'], width=80)  
                     col2.write(track['name'])
+
                     if track['preview_url'] is not None:
-                        #col11.write(track['preview_url'])  
-                        #col11.image(track['album']['images'][0]['url'], width=80)  
                         with col11:   
                             st.audio(track['preview_url'], format="audio/mp3")  
                     with col3:
-                        def feature_requested():
-                            track_features  = spotify.audio_features(track['id']) 
-                            df = pd.DataFrame(track_features, index=[0])
-                            df_features = df.loc[: ,['danceability', 'energy', 'instrumentalness', 'speechiness', 'liveness', 'loudness', 'acousticness', 'valence', 
-                            'duration_ms', 'key', 'mode', 'tempo', 'time_signature']]
-                            track_info = spotify.track(track['id'])
-                            df_track_info = pd.json_normalize(track_info)[['explicit', 'popularity']]
-                            df_track_followers = pd.DataFrame(data=artist_followers, columns=['followers_artist_1'])
-                            df_features_final = pd.concat([df_features, df_track_followers, df_track_info], axis=1)
-                            with col21:
-                                st.dataframe(df_features_final)
-                            return df_features_final
-                        df_features_final[track['id']] = feature_requested()                       
+                        track_features  = spotify.audio_features(track_id) 
+                        df = pd.DataFrame(track_features, index=[0])
+                        df_features = df.loc[: ,['danceability', 'energy', 'instrumentalness', 'speechiness', 'liveness', 'loudness', 'acousticness', 'valence', 
+                        'duration_ms', 'key', 'mode', 'tempo', 'time_signature']]
 
-                        #with col31:
-                        #    polarplot.feature_plot(df_features)
-                            #feature_button_state = st.button('Predecir', key=track['id'], on_click=predict)
-            st.button('predecir', key=track['id'], on_click=predict(df_features_final[track['id']]))
-                                                   
-                    #    feature_button_state = st.button('Track Audio Features', key=track['id'], on_click=feature_requested)
+                        df_track_info = pd.DataFrame(data= {'explicit': [track['explicit']], 
+                                                            'popularity': [track['popularity']], 
+                                                            'release_date': [track['album']['release_date']]})
+                        df_track_info['release_year'] = df_track_info['release_date'].str[0:4]
 
+                        df_track_followers = pd.DataFrame(data=[artist_followers], columns=['followers_artist_1'])
+                        df_features_final = pd.concat([df_features, df_track_followers, df_track_info[['explicit', 'popularity', 'release_year']]], axis=1)
+                
+                        def predict():
+                            # llamada a modelo de prediccion
+                            predict_model(df_features_final)
 
-
-    def features():
-        data ={
-            'danceability': [0.878],
-            'energy': [0.575],
-            'instrumentalness': [0.00000],
-            'speechiness': [0.2840],
-            'liveness': [0.2730],
-            'explicit': [True],
-            'loudness': [-8732],
-            'valence': [0.3990],
-            'tempo': [144918],
-            'popularity_artist_1': [86],
-            'key_0.0': [0],
-            'key_1.0': [0],
-            'key_2.0': [0],
-            'key_3.0': [0],
-            'key_4.0': [0],
-            'key_5.0': [0],
-            'key_6.0': [0],
-            'key_7.0': [0],
-            'key_8.0': [0],
-            'key_9.0': [1],
-            'key_10.0': [0],
-            'key_11.0': [0],
-            'mode_0.0': [0],
-            'mode_1.0': [1],
-            'time_signature_0.0': [0],
-            'time_signature_1.0': [0],
-            'time_signature_3.0': [0],
-            'time_signature_4.0': [1],
-            'time_signature_5.0': [0],
-            'duration_m': [3086883]
-            }
-        features = pd.DataFrame(data, index=[0])
-        #st.success(gb_clf.predict(df))
-        return features
+                        with col21:
+                            st.dataframe(df_features_final) 
+                            feature_button_state = st.button('predecir', key=track_id, on_click=predict)
 
 
-    
-    #if st.button('RUN'):
-   
-def predict(df_features_final):
-    print('************* entra predict')
-    df_features = prepare_feature(df_features_final)
+# metodo de llamada al modelo de prediccion   
+def predict_model(df):  
+    features = prepare_feature(df)
 
     with st.sidebar.container():
-        st.success(gb_clf.predict(df_features_final))     
+        st.success(classify(model.predict(features))) 
 
-def prepare_feature(df_features_final):
-    df_features = df_features_final
+# metodo de traduccion del resultado del modelo   
+def classify(num):
+    if num == '0':
+        return 'Popularidad BAJA'
+    elif num == '1':
+        return 'Popularidad MEDIA'
+    elif num == '2':
+        return 'Popularidad ALTA'
+    else: return 'Valor no esperado'  
+
+# metodo de preparacion de dataset para la prediccion
+def prepare_feature(df):
+    
+    df_features = df
+
+    df_features = df_features.drop(['popularity'], axis=1) 
 
     df_features['duration_m'] = round(df_features['duration_ms']/60000, 2)
     df_features = df_features.drop(['duration_ms'], axis=1)
 
-    df_features['key'] = df_features['key'].astype('category')
-    df_features['mode'] = df_features['mode'].astype('category')
-    df_features['time_signature'] = df_features['time_signature'].astype('category')
+    df_features['key_0.0'] = np.where(df_features['key']== 0, 1, 0)
+    df_features['key_1.0'] = np.where(df_features['key']== 1, 1, 0)
+    df_features['key_2.0'] = np.where(df_features['key']== 2, 1, 0)
+    df_features['key_3.0'] = np.where(df_features['key']== 3, 1, 0)
+    df_features['key_4.0'] = np.where(df_features['key']== 4, 1, 0)
+    df_features['key_5.0'] = np.where(df_features['key']== 5, 1, 0)
+    df_features['key_6.0'] = np.where(df_features['key']== 6, 1, 0)
+    df_features['key_7.0'] = np.where(df_features['key']== 7, 1, 0)
+    df_features['key_8.0'] = np.where(df_features['key']== 8, 1, 0)
+    df_features['key_9.0'] = np.where(df_features['key']== 9, 1, 0)
+    df_features['key_10.0'] = np.where(df_features['key']== 10, 1, 0)
+    df_features['key_11.0'] = np.where(df_features['key']== 11, 1, 0)
+    df_features = df_features.drop(['key'], axis=1) 
 
-    df_features = pd.get_dummies(df_features)
+    df_features['mode_0.0'] = np.where(df_features['mode']== 0, 1, 0)
+    df_features['mode_1.0'] = np.where(df_features['mode']== 1, 1, 0)
+    df_features = df_features.drop(['mode'], axis=1) 
 
-    print(df_features.columns)
+    df_features['time_signature_0.0'] = np.where(df_features['time_signature']== 0, 1, 0)
+    df_features['time_signature_1.0'] = np.where(df_features['time_signature']== 1, 1, 0)
+    df_features['time_signature_3.0'] = np.where(df_features['time_signature']== 3, 1, 0)
+    df_features['time_signature_4.0'] = np.where(df_features['time_signature']== 4, 1, 0)
+    df_features['time_signature_5.0'] = np.where(df_features['time_signature']== 5, 1, 0)
+    df_features = df_features.drop(['time_signature'], axis=1) 
 
-    return df_features
+    fields = ['danceability', 'energy', 'instrumentalness', 'speechiness', 'liveness',
+       'explicit', 'loudness', 'valence', 'tempo', 'followers_artist_1',
+       'release_year', 'key_0.0', 'key_1.0', 'key_2.0', 'key_3.0', 'key_4.0',
+       'key_5.0', 'key_6.0', 'key_7.0', 'key_8.0', 'key_9.0', 'key_10.0',
+       'key_11.0', 'mode_0.0', 'mode_1.0', 'time_signature_0.0',
+       'time_signature_1.0', 'time_signature_3.0', 'time_signature_4.0',
+       'time_signature_5.0', 'duration_m']
+
+    return (df_features[fields].to_numpy())
 
 if __name__ == '__main__':
     main()
